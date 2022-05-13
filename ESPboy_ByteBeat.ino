57ddda9702de262435413b96202d71e0f35f3baa
ESPboy <ESPboy.edu@gmail.com>
2022-05-13 22:45:29 +0300

    add melodies

--- ESPboy_ByteBeat.ino
+++ ESPboy_ByteBeat.ino
@@ -19,6 +19,7 @@ http://langtons.atspace.com/audio/waveform.html
 #include<sigma_delta.h>
 
 #define SAMPLE_RATE 8000
+#define MELODIES_NO 14
 
 ESPboyInit myESPboy;
 volatile uint8_t functionNo=0;
@@ -28,7 +29,17 @@ String musicFormulas[]=
   "((t<<1)^((t<<1)+(t>>7)&t>>12))|t>>(4-(1^7&(t>>19)))|t>>7",
   "((t*((t>>8)|(t>>18))&33&t>>3))^((t&t>>8)|(t>>20))",
   "(((t>>7)|((t&t)>1))|(t&t>>8)|(t>>7))|(t+(t>>8))|((t>>7))",
-  "((t>>6)^(t&0x25))|((t+(t^(t>>11))-t* ((t%24?2:6)&t>>11))^(t<<1&(t&0x256?t>>4:t>>10)))"
+  "((t>>6)^(t&0x25))|((t+(t^(t>>11))-t* ((t%24?2:6)&t>>11))^(t<<1&(t&0x256?t>>4:t>>10)))",
+  "((t<<1)^((t<<1)+(t>>7)&t>>12))|t>>(4-(1^7&(t>>19)))|t>>7",
+  "((t>>10)&42)*t",
+  "((t*5&t>>7)|(t*3&t>>10))",
+  "((t>>6|t<<1)+(t>>5|t<<3|t>>3)|t>>2|t<<1)",
+  "((t*((3+(1^t>>10&5))*(5+(3&t>>14))))>>(t>>8&3))",
+  "((t*9&t>>4|t*5&t>>7|t*3&t/1024)-1)",
+  "((t>>6|t|t>>(t>>16))*10+((t>>11)&7))",
+  "(t*((t>>12|t>>8)&63&t>>4))",
+  "((t*((t>>9|t>>13)&15))&129)",
+  "(t*(t>>((t&4096)&&((t*t)/4096)||(t/4096)))|(t<<(t/256))|(t>>4))",
  };
 
 
@@ -52,6 +63,37 @@ void IRAM_ATTR byteBeatStepISR(){
    case 4:
      beatByte = ((t>>6)^(t&0x25))|((t+(t^(t>>11))-t* ((t%24?2:6)&t>>11))^(t<<1&(t&0x256?t>>4:t>>10)));
      break;
+   case 5:
+     beatByte = ((t<<1)^((t<<1)+(t>>7)&t>>12))|t>>(4-(1^7&(t>>19)))|t>>7;
+   break;
+   case 6:
+     beatByte = ((t >> 10) & 42) * t;
+   break;
+   case 7:
+     beatByte = ((t*5&t>>7)|(t*3&t>>10));
+   break;
+   case 8:
+     beatByte = ((t>>6|t<<1)+(t>>5|t<<3|t>>3)|t>>2|t<<1);
+   break;
+   case 9:
+     beatByte = ((t*((3+(1^t>>10&5))*(5+(3&t>>14))))>>(t>>8&3));
+   break;
+   case 10:
+     beatByte = ((t*9&t>>4|t*5&t>>7|t*3&t/1024)-1);
+   break;
+   case 11:
+     beatByte = ((t>>6|t|t>>(t>>16))*10+((t>>11)&7));
+   break;
+   case 12:
+     beatByte = (t*((t>>12|t>>8)&63&t>>4));
+   break;
+   case 13:
+     beatByte = ((t*((t>>9|t>>13)&15))&129);
+   break;
+   case 14:
+     beatByte = (t*(t>>((t&4096)&&((t*t)/4096)||(t/4096)))|(t<<(t/256))|(t>>4));
+   break;
+     
  }
  sigmaDeltaWrite(0, beatByte);
 }
@@ -107,7 +149,7 @@ void loop() {
  uint8_t keys=myESPboy.getKeys();
  if (keys){
    if(keys&PAD_DOWN && functionNo > 0) functionNo--;
-   if(keys&PAD_UP && functionNo < 4) functionNo++;
+   if(keys&PAD_UP && functionNo < MELODIES_NO) functionNo++;
    drawInfo();
  }
  delay(200);
